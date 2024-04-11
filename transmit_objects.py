0cd28561b6e9341cde8cc2412caca9b6f18e6b13
Cheikh Sidiya <cheikhsidiya55@gmail.com>
2024-04-11 03:19:53 +0000

    fixing bugs

--- transmit_objects.py
+++ transmit_objects.py
@@ -2,6 +2,14 @@ import socket
 import pickle
 import re
 import sys
+from time import sleep
+
+
+class ObjectReceiveError(Exception):
+    """Raised when the obj is not received properly."""
+
+    __str__ = lambda _: "Couldn't Receive Object properly"
+
 
 port_pattern = r"^[0-9]{1,5}$"
 
@@ -17,50 +25,74 @@ def check_address(addr: tuple) -> bool:
     )
 
 
+def get_tcp_sock():
+    return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
 class Client:
-    def __init__(self, *, bind_addr: tuple) -> None:
+    def __init__(self, bind_addr: tuple) -> None:
 
         if check_address(bind_addr):
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.bind(bind_addr)
+            self.bind_addr = bind_addr
+            self.socket = get_tcp_sock()
 
     def send_object(self, addr: tuple, obj) -> None:
 
         if isinstance(addr, tuple):
-            self.socket.connect(addr)
+            while True:
+                try:
+                    self.socket.connect(addr)
+                    break
+                except ConnectionRefusedError:
+                    continue
+            try:
+                # Send size
+                obj_size = sys.getsizeof(obj).to_bytes(8, byteorder="big")
+                self.socket.sendall(obj_size)
 
-            # Send size
-            obj_size = str(sys.getsizeof(obj)).encode()
-            self.socket.sendall(obj_size)
+                # Send obj
+                self.socket.sendall(pickle.dumps(obj) + b"EOF")
+                sleep(0.15)  # A small delay to address race conditions
 
-            # Send obj
-            self.socket.sendall(pickle.dump(obj))
-
-            self.socket.close()  # Reset socket
+            finally:
+                # Reset socket
+                self.socket.close()
+                self.socket = get_tcp_sock()
 
     def recv_obj(self, addr: tuple) -> None | object:
         if check_address(addr):
-            self.socket.listen()
-            conn, _ = self.socket.accept()
+            try:
+                self.socket.bind(self.bind_addr)
+                self.socket.listen()
+                conn, _ = self.socket.accept()
 
-            # Receive size
-            obj_size = int(conn.recv(1024).decode())
+                # Receive size
+                obj_size = int.from_bytes(conn.recv(8), "big")
 
-            # Receive object
-            received_obj = b""
-            while len(received_obj) < obj_size:
-                chunk_size = min(obj_size - len(received_obj), 1024)
-                chunk = conn.recv()
-                if not chunk:
-                    break
-                received_obj += chunk
+                # Receive object
+                received_obj = b""
+                while len(received_obj) < obj_size:
+                    chunk_size = min(obj_size - len(received_obj), 1024)
+                    chunk = conn.recv(chunk_size)
+                    if not chunk and received_obj.endswith(b"EOF"):
+                        break
+                    received_obj += chunk
 
-            conn.close()
+                if received_obj.endswith(b"EOF"):
+                    received_obj = received_obj[:-3]
 
-            return pickle.loads(received_obj)
+                else:
+                    raise ObjectReceiveError()
+
+                conn.close()
+                return pickle.loads(received_obj)
+
+            finally:
+                # Reset the socket
+                self.socket.close()
+                self.socket = get_tcp_sock()
 
     # Context managing protocol
-
     def __enter__(self):
         return self
 
